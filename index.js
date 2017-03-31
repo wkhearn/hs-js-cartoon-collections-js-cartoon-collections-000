791562d0afc899851249eb421f3a9ea5132eaeef
Kenny Hearn <wkhearn@gmail.com>
2017-03-31 02:10:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){
+  var newArray = []
+  for (var i = 0; i < dwarves.length; i++){
+    newArray += `${i+1}. ${dwarves[i]} `
+  }
+  return newArray
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newArray = []
+  for (var i = 0; i < planeteerCalls.length; i++){
+    newArray.push(`${planeteerCalls[i]}!`.toUpperCase())
+  }
+  return newArray
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (foods){
+  for (var i = 0; i < foods.length; i++){
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert"){
+      return foods[i]
+    } else {}
+  } return "no cheese!"
 }
